fc6634590436816f44ad219e63d07f7a641ccc24
weblxg2015 <weblxg2015@163.com>
2015-07-09 23:52:40 +0800

    7月9

---
+++ 7月9日/js/function.js
@@ -0,0 +1,11 @@
+/**
+ * Created by q4156_000 on 2015/7/9.
+ */
+Array.prototype.zwsort=function(){
+    function funzwsort(a,b) {
+        {
+             a.localeCompare(b);
+        }
+    }
+    this.sort(funzwsort);
+}
